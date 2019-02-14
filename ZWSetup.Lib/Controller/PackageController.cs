1f9965a1fd4849caa8600ed0cb8393ad15c44290
Ikillnukes <ikillnukes1@gmail.com>
2019-02-14 13:16:20 +0100

    Moved Controller to Lib

---
+++ ZWSetup.Lib/Controller/PackageController.cs
@@ -0,0 +1,17 @@
+﻿namespace ZWSetup.Lib.Controller
+{
+    using Lib.Model;
+
+    public static class PackageController
+    {
+        public static ZTWPackage CurrentPackage => SelectedPackage != -1 ? PackageList[SelectedPackage] : null;
+        public static int SelectedPackage { get; set; }
+        public static PackageList PackageList { get; private set; }
+
+        static PackageController()
+        {
+            PackageList = new PackageList();
+            SelectedPackage = -1;
+        }
+    }
+}
--- ZWSetup.Shell/Pages/PackageInstaller.cs
+++ ZWSetup.Shell/Pages/PackageInstaller.cs
@@ -4,10 +4,9 @@ using System.Collections.Generic;
 namespace ZWSetup.Shell.Pages
 {
     using Extensions;
-    using Lib.Model;
     using Packages;
     using FeatureExpansion;
-    using Controller;
+    using Lib.Controller;
 
     public class PackageInstaller : UpdatableMenuPage
     {
@@ -24,16 +23,15 @@ namespace ZWSetup.Shell.Pages
         public static IEnumerable<Option> GetOptions(UpdatableProgram program)
         {
             int i = 0;
-            ZTWPackage package = null;
 
             foreach (var pkg in PackageController.PackageList)
             {
-                yield return new Option(pkg.Name, () => package = PackageController.PackageList[i]);
+                yield return new Option(pkg.Name, () => PackageController.SelectedPackage = i);
                 ++i;
             }
 
-            yield return new Option("New package", () => program.GetPage<PackageAdd>().SetPackage(package).NavigateTo<PackageAdd>());
-            yield return new Option("Remove package", () => program.GetPage<PackageRemove>().SetPackage(package).NavigateTo<PackageRemove>());
+            yield return new Option("New package", () => program.GetPage<PackageAdd>().SetPackage(PackageController.CurrentPackage).NavigateTo<PackageAdd>());
+            yield return new Option("Remove package", () => program.GetPage<PackageRemove>().SetPackage(PackageController.CurrentPackage).NavigateTo<PackageRemove>());
         }
     }
 }
