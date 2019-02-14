f365eeeb81cfc62ddd8c2846a5ebc5829c2396a5
Ikillnukes <ikillnukes1@gmail.com>
2019-02-14 12:31:03 +0100

    Added Controller

---
+++ ZWSetup.Shell/Controller/PackageController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZWSetup.Shell.Controller
+{
+    using Lib;
+
+    public static class PackageController
+    {
+        public static PackageList PackageList { get; private set; }
+
+        static PackageController()
+        {
+            PackageList = new PackageList();
+        }
+    }
+}
