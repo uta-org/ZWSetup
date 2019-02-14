5eb78f35e246c194c33b5cd417802a3981719cdf
Ikillnukes <ikillnukes1@gmail.com>
2019-02-14 12:29:07 +0100

    Added model

---
+++ ZWSetup.Lib/PackageList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZWSetup.Lib
+{
+    using Model;
+
+    public sealed class PackageList : List<ZTWPackage>
+    {
+    }
+}
