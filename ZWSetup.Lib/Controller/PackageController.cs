d4f34e05d94d2920398b810ae94b25992f516b5e
Ikillnukes <ikillnukes1@gmail.com>
2019-02-14 13:27:04 +0100

    Impl Add && Removed Package Display methods

--- ZWSetup.Lib/Controller/PackageController.cs
+++ ZWSetup.Lib/Controller/PackageController.cs
@@ -13,5 +13,40 @@
             PackageList = new PackageList();
             SelectedPackage = -1;
         }
+
+        /// <summary>
+        /// Adds a new package following the specified path.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <returns></returns>
+        public static ZTWPackage Add(string path)
+        {
+            var package = new ZTWPackage(path);
+            PackageList.Add(package);
+
+            return package;
+        }
+
+        /// <summary>
+        /// Removes the current package.
+        /// </summary>
+        /// <returns>The package name.</returns>
+        public static string RemoveCurrent()
+        {
+            string name = CurrentPackage.Name;
+            PackageList.Remove(CurrentPackage);
+            SelectedPackage = -1;
+
+            return name;
+        }
+
+        /// <summary>
+        /// Removes the specified package by its index.
+        /// </summary>
+        /// <param name="index">The index.</param>
+        public static void Remove(int index)
+        {
+            PackageList.RemoveAt(index);
+        }
     }
 }
